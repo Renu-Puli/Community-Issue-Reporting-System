258b0e0adcf7350bc243fbbd931c8ca50367498f
Renu Puli <renupuli99510@gmail.com>
2026-05-12 14:04:33 +0530

    fixed vite build command

--- community2/Backend/server.js
+++ community2/Backend/server.js
@@ -14,7 +14,12 @@ connectDB();
 const app = express();
 
 app.use(cors({
-    origin: ["http://localhost:5173", "http://localhost:8080", "http://localhost:3000"],
+    origin: [
+ "http://localhost:5173",
+ "http://localhost:8080",
+ "http://localhost:3000",
+ "https://community-issue-reporting-system-inky.vercel.app"
+],
     credentials: true,
 }));
 app.use(express.json({ limit: "50mb" }));
