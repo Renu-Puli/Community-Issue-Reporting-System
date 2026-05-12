83392f4418dbe08815656f6068cc5c67f32cd000
Renu Puli <renupuli99510@gmail.com>
2026-05-12 16:43:40 +0530

    updated cors

--- community2/Backend/server.js
+++ community2/Backend/server.js
@@ -15,10 +15,7 @@ const app = express();
 
 app.use(cors({
     origin: [
- "http://localhost:5173",
- "http://localhost:8080",
- "http://localhost:3000",
- "https://community-issue-reporting-system-inky.vercel.app"
+ "https://community-issue-reporting-system-1.onrender.com"
 ],
     credentials: true,
 }));
